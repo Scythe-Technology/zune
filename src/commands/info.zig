816887d490f13693c0643ba2b909e0aad7509fa4
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-09-03 18:19:12 -0400

    add abi into platform info

--- src/commands/info.zig
+++ src/commands/info.zig
@@ -14,7 +14,7 @@ fn Execute(_: std.mem.Allocator, args: []const []const u8) !void {
     Zune.debug.print("<bold>version<clear>:  {s}\n", .{zune_info.version});
     Zune.debug.print("<bold>luau<clear>:     {d}.{d}\n", .{ LUAU_VERSION.major, LUAU_VERSION.minor });
     Zune.debug.print("<bold>async<clear>:    <green>{t}<clear>\n", .{xev.backend});
-    Zune.debug.print("<bold>platform<clear>: {t}-{t}\n", .{ builtin.cpu.arch, builtin.os.tag });
+    Zune.debug.print("<bold>platform<clear>: {t}-{t}-{t}\n", .{ builtin.cpu.arch, builtin.os.tag, builtin.abi });
     Zune.debug.print("<bold>threaded<clear>: {} (<green>{}<clear> threads)\n", .{
         !builtin.single_threaded,
         if (builtin.single_threaded) 1 else std.Thread.getCpuCount() catch 1,
